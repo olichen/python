8744f7e7d4246cfc172be659a3502ed36937d92b
Oliver Chen <olichen@ucdavis.edu>
2020-05-16 11:50:41 -0700

    csv input

--- liquidhandler/test_handler.py
+++ liquidhandler/test_handler.py
@@ -1,4 +1,6 @@
 from opentrons import protocol_api
+import csv
+import re
 
 metadata = {'apiLevel': '2.2'}
 
@@ -8,3 +10,19 @@ def run(protocol: protocol_api.ProtocolContext):
     p300 = protocol.load_instrument('p300_single', 'right', tip_racks=[tiprack_1])
 
     p300.transfer(100, plate['A1'], plate['B1'])
+
+
+with open('SingleHeadTransfer.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    wells = [[[0] * 12] * 8]
+    regex = re.compile('[a-zA-Z][0-9]')
+    for row in reader:
+        well = row[0].strip()
+        volume = row[1].strip()
+        print(well)
+        if not regex.match(well):
+            print('Skipping well "' + well + '"')
+            continue
+        print(well + ' ' + volume)
+        # wells[int(well[1])-1][0] = row[1]
+    print(wells)
