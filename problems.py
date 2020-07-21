ed93f72b622e9c86b756bc515b1432fa60939c74
Oliver Chen <olichen@ucdavis.edu>
2020-07-21 13:09:08 -0700

    more problems

--- problems.py
+++ problems.py
@@ -1,3 +1,45 @@
+# For an M x N matrix, if an element is 0 set its entire row/column to 0
+from typing import List
+
+def zeroMatrix(A: List[list]) -> None:
+    M = len(A)
+    N = len(A[0])
+    m_zero = [False] * M
+    n_zero = [False] * N
+    for i in range(M):
+        for j in range(N):
+            if A[i][j] == 0:
+                m_zero[i] = True
+                n_zero[j] = True
+                continue
+
+    for i, zero in enumerate(m_zero):
+        if zero:
+            for j in range(N):
+                A[i][j] = 0
+
+    for j, zero in enumerate(n_zero):
+        if zero:
+            for i in range(M):
+                A[i][j] = 0
+
+
+# In place rotation of an N x N matrix A by 90 degrees
+from typing import List
+
+def rotateMatrix(A: List[list]) -> None:
+    N = len(A[0])
+    for y in range(int(N/2)):
+        ep = N - y - 1 # end pixel
+        for x in range(y, ep):
+            print(str(y) + ' ' + str(x))
+            temp = A[y][y+x]
+            A[y][y+x] = A[y+x][ep]
+            A[y+x][ep] = A[ep][ep-x]
+            A[ep][ep-x] = A[ep-x][y]
+            A[ep-x][y] = temp
+
+
 # Basic string compression ('aabbbc' => 'a2b3c')
 def stringCompression(s: str) -> str:
     out = ''
